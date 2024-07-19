0a664564d06e609b7df1ad546f6f59982776f770
Gaurav Tiwari <gauravkrtiwari79@gmail.com>
2024-07-19 12:13:36 +0530

    add reset router and include userid in returned token

--- controllers/login.js
+++ controllers/login.js
@@ -9,7 +9,7 @@ loginRouter.post('/', async (request, response) => {
   const user = await User.findOne({ username });
   const passwordCorrect = user === null
     ?false
-    :bcrypt.compare(password, user.passwordHash);
+    :await bcrypt.compare(password, user.passwordHash);
 
   if(!user || !passwordCorrect){
     return response.status(401).json({
@@ -27,7 +27,7 @@ loginRouter.post('/', async (request, response) => {
 
   response
     .status(200)
-    .send({ token, username: user.username, name: user.name });
+    .send({ token, username: user.username, name: user.name, id: user._id });
 });
 
 module.exports = loginRouter;
